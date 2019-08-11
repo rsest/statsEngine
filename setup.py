4436e9e2a8a84dbeaf7f75c5b37cc0381da20bdc
Rodrigo Sestari <rodrigo.sestari@klar.mx>
2019-08-11 14:39:06 +0200

    review lib

--- setup.py
+++ setup.py
@@ -7,17 +7,21 @@ from statistics import __version__ as version
 description = 'Python Statistical Engine.'
 
 setuptools.setup(
-        name='statsEngine',
-        version=version,
-        description='{0}'.format(description),
-        author='Rodrigo Sestari',
-        url='https://github.com/rsest/statsEngine',
-        packages=['statsEngine'],
-        license='MIT',
-        classifiers=[
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: BSD License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 3.6',
-        ],
+    name='statsEngine',
+    version=version,
+    description='{0}'.format(description),
+    author='Rodrigo Sestari',
+    url='https://github.com/rsest/statsEngine',
+    packages=['statsEngine'],
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ], install_requires=["numpy" == 1.17.0,
+                         "pandas" == 0.24.0,
+                         "scipy" == 1.3.1,
+                         "matplotlib" == 3.1.1,
+                         "plotly" == 4.1.0],
 )
