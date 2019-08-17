3b006df464218921b8f9e6e807f4abd1a00460f0
Rodrigo Sestari <rodrigo.sestari@alumni.unitn.it>
2019-08-17 12:13:50 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@
 
 import setuptools
 
-from statisticEngine import __version__ as version
-
 description = 'Python Statistical Engine.'
 
 setuptools.setup(
+    install_requires=["numpy==1.17.0",
+                      "pandas==0.24.0",
+                      "scipy==1.3.1",
+                      "matplotlib==3.1.1",
+                      "plotly==4.1.0"],
     name='statisticEngine',
-    version=version,
+    version='0.0.1',
     description='{0}'.format(description),
     author='Rodrigo Sestari',
     url='https://github.com/rsest/statsEngine',
@@ -19,9 +22,5 @@ setuptools.setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
-    ], install_requires=["numpy==1.17.0",
-                         "pandas==0.24.0",
-                         "scipy==1.3.1",
-                         "matplotlib==3.1.1",
-                         "plotly==4.1.0"],
+    ]
 )
